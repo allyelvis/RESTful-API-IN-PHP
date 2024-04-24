789363439b6a9890c5cedc99f3fd196b539bf8be
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-04-24 13:41:22 +0200

    Create config.php
    Handle CORS

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
