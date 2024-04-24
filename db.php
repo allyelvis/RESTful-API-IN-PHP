fac62bee0613133c4bfd3170c64a5633a3ca7f1a
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-04-24 13:37:35 +0200

    Create db.php
    create databse

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'localhost';
+$username = 'your_username';
+$password = 'your_password';
+$dbname = 'your_database';
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
